a18f6e21a4974f766c2816d3344a32b70502eea1
Victor Kinelev <victor.kinelev.1@gmail.com>
2018-06-20 18:26:51 +0200

    wip: prime testing algorithms - benchmarks

--- app/lib/primality/algorithms/optimized_trial_division.rb
+++ app/lib/primality/algorithms/optimized_trial_division.rb
@@ -1,8 +1,25 @@
 module Primality
   module Algorithms
-    class OptimizedTrialDivision  
+    class OptimizedTrialDivision
+
+      PRIMES_LESS_THAN_100 =
+
+      def self.primes_less_than_100
+        @primes_less_than_100 ||= [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
+         71, 73, 79, 83, 89, 97].reduce(Hash.new) { |hash, number| hash[number] = true }
+      end
+
       def prime?(number)
-        true 
+        return false if number <= 1
+        return true if number == 2 || number == 3
+
+        k = 2
+        while k < Math.sqrt(number)
+          return false if number % k == 0
+          k += 1
+        end
+
+        true
       end
     end
   end
--- test/lib/primality/algorithms/optimized_trial_division_test.rb
+++ test/lib/primality/algorithms/optimized_trial_division_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require "minitest/benchmark"
 
 class OptimizedTrialDivisionTest < ActiveSupport::TestCase
   def setup
@@ -6,10 +7,53 @@ class OptimizedTrialDivisionTest < ActiveSupport::TestCase
   end
 
   test 'it returns true if a prime number has been passed' do
-    assert algorithm.prime?(2)
+    assert algorithm.prime?(9999991)
+  end
+
+  test 'it returns true if a composite number has been passed' do
+    refute algorithm.prime?(9999999)
   end
 
   private
 
   attr_reader :algorithm
 end
+
+class OptimizedTrialDivisionBenchmark < Minitest::Benchmark
+  def setup
+    @algorithm = Primality::Algorithms::OptimizedTrialDivision.new
+  end
+
+  protected
+
+  attr_reader :algorithm
+end
+
+class LargePrimes < OptimizedTrialDivisionBenchmark
+  def bench_large_primes
+    assert_performance_power 0.9999 do |n| # n is a range value
+      10000.times { algorithm.prime?(n) }
+    end
+  end
+
+  def self.bench_range
+    [28087, 104729, 1299439, 9999991]
+  end
+end
+
+class NumbersLessThan100 < OptimizedTrialDivisionBenchmark
+  def bench_number_less_than_100
+    assert_performance_constant 0.9999 do |n| # n is a range value
+      10000.times { algorithm.prime?(n) }
+    end
+  end
+
+  def self.bench_range
+    [2, 29, 61, 97]
+  end
+end
+
+
+
+# bench_number_less_than_100	 0.000683	 0.004482	 0.004723	 0.005925
+# bench_large_primes	 0.094473	 0.180707	 0.633801	 1.754694
